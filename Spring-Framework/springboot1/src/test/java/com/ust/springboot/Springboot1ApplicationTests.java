7d9324fa63fc228e5afd9066b15fb1b69ff45152
Pavith Sudhir <56886753+PavithSudhirAP@users.noreply.github.com>
2022-04-22 12:50:46 +0530

    Add files via upload

---
+++ Spring-Framework/springboot1/src/test/java/com/ust/springboot/Springboot1ApplicationTests.java
@@ -0,0 +1,25 @@
+package com.ust.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ust.springboot.services.Service;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Autowired 
+	ApplicationContext context;
+	
+	@Test
+	void testService() {
+		Service s=context.getBean(Service.class);
+		s.save();
+	}
+
+}
